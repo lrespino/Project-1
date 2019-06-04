601e0c71d03bcd1a5f6d573a571a427c236843e7
Kayleighs <47003302+kayleighs@users.noreply.github.com>
2019-06-04 12:43:12 -0400

    test drag and drop

---
+++ assets/JavaScript/drag-and-drop-logic.js
@@ -0,0 +1,24 @@
+ $(document).ready(function () {
+    console.log("ready!");
+
+    var box = document.getElementsByClassName('box')[0]
+    var containers = document.getElementsByClassName('holder')
+    for (var container of containers) {
+        container.addEventListener("dragover", dragover)
+        container.addEventListener("dragenter", dragenter)
+        container.addEventListener("drop", drop)
+    }
+
+    function dragover(e) {
+        e.preventDefault()
+    }
+    function dragenter(e) {
+        e.preventDefault()
+    }
+    function drop() {
+        this.append(box)
+    } 
+
+});
+
+
